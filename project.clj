0983eaf2684ced3e655a3ed480648cd443443816
Chris Howe-Jones <chris.howejones@gmail.com>
2015-01-30 10:26:06 +0000

    Updated project description.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject data-generator "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "CSV trade file data generator."
+  :url "https://github.com/chrishowejones/data-generator"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
